44cd9da95ced3cc4ef4093d02407d47d4715932e
Rongjian Zhang <pd4d10@gmail.com>
2018-08-14 22:05:34 +0800

    Recreate with react-native init

--- android/app/src/main/java/com/lamernews/MainApplication.java
+++ android/app/src/main/java/com/lamernews/MainApplication.java
@@ -1,4 +1,4 @@
-package com.echojs;
+package com.lamernews;
 
 import android.app.Application;
 
--- ios/LamerNewsTests/LamerNewsTests.m
+++ ios/LamerNewsTests/LamerNewsTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface echojsTests : XCTestCase
+@interface LamerNewsTests : XCTestCase
 
 @end
 
-@implementation echojsTests
+@implementation LamerNewsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
