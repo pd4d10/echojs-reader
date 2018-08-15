3fffdaa6fa1082aac62449abbad1d61ecc545c72
Rongjian Zhang <pd4d10@gmail.com>
2018-08-15 15:25:16 +0800

    Merge storage get at first time

--- app/index.js
+++ app/index.js
@@ -18,39 +18,49 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
-    this.getLayout()
-    this.getTheme()
+    this.getInitialSettings()
   }
 
-  getLayout = async () => {
-    let layout = await AsyncStorage.getItem(STORAGE_KEYS.layout)
-    if (!Object.keys(layoutMapping).includes(layout)) {
-      layout = Platform.OS === 'android' ? 'drawer' : 'bottom-tab'
+  getInitialSettings = async () => {
+    let [layout, theme] = await AsyncStorage.multiGet([
+      STORAGE_KEYS.layout,
+      STORAGE_KEYS.theme,
+    ])
+    layout = this.ensureLayoutCorrect(layout)
+    theme = this.ensureThemeCorrect(theme)
+    this.setState({ layout, theme })
+  }
+
+  ensureLayoutCorrect = layout => {
+    if (Object.keys(layoutMapping).includes(layout)) {
+      return layout
+    } else {
+      return Platform.OS === 'android' ? 'drawer' : 'bottom-tab'
     }
-    this.setState({ layout })
+  }
+
+  ensureThemeCorrect = theme => {
+    if (Object.keys(themeMapping).includes(theme)) {
+      return theme
+    } else {
+      return 'light'
+    }
   }
 
   setLayout = async layout => {
     await AsyncStorage.setItem(STORAGE_KEYS.layout, layout)
+    layout = this.ensureLayoutCorrect(layout)
     this.setState({ layout })
-  }
-
-  getTheme = async () => {
-    let theme = await AsyncStorage.getItem(STORAGE_KEYS.theme)
-    if (!Object.keys(themeMapping).includes(theme)) {
-      theme = 'light'
-    }
-    this.setState({ theme })
   }
 
   setTheme = async theme => {
     await AsyncStorage.setItem(STORAGE_KEYS.theme, theme)
-    await this.getTheme()
+    theme = this.ensureThemeCorrect(theme)
+    this.setState({ theme })
   }
 
   render() {
     const { layout, theme } = this.state
-    // console.log(layout)
 
     if (!layout || !theme) {
       return null
