5e18b18b5f11e43b2d1110100f192b9893743194
Rongjian Zhang <pd4d10@gmail.com>
2019-07-06 15:56:50 +0800

    build: react-native 0.60

--- android/app/src/main/java/io/github/pd4d10/echojsreader/MainApplication.java
+++ android/app/src/main/java/io/github/pd4d10/echojsreader/MainApplication.java
@@ -2,16 +2,13 @@ package io.github.pd4d10.echojsreader;
 
 import android.app.Application;
 
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -24,12 +21,14 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNGestureHandlerPackage(),
-            new AsyncStoragePackage(),
-            new VectorIconsPackage()
-      );
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new MyReactNativePackage());
+
+      // https://github.com/kmagiera/react-native-gesture-handler/issues/494#issuecomment-471164413
+      packages.add(new RNGestureHandlerPackage());
+      return packages;
     }
 
     @Override
