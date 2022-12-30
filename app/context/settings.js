bc8e18fe3b8ed3b49a0559083bfed2b536c7ba69
Rongjian Zhang <pd4d10@gmail.com>
2022-12-30 23:46:27 +0800

    refactor: use expo-web-browser

--- app/context/settings.js
+++ app/context/settings.js
@@ -1,51 +1,21 @@
 import React from "react";
-import { Linking } from "react-native";
+import { Linking, Platform } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import SafariView from "react-native-safari-view";
+import * as WebBrowser from "expo-web-browser";
 import { STORAGE_KEYS } from "../constants";
 
 export const SettingsContext = React.createContext();
 
 export const SettingsProvider = ({ children }) => {
-  const [svAvailable, setSvAvailable] = React.useState(false);
+  const svAvailable = Platform.OS === "ios";
   const [svEnable, _setSvEnable] = React.useState(false);
-  const [inSv, setInSv] = React.useState(false);
-  const [svStarted, setSvStarted] = React.useState(false);
 
   React.useEffect(() => {
-    const handleShow = () => {
-      setInSv(true);
-    };
-    const handleClose = () => {
-      setInSv(false);
-      setSvStarted(false);
-    };
-
     (async () => {
       let enable = await AsyncStorage.getItem(STORAGE_KEYS.safariView);
-
-      let available;
-      try {
-        available = await SafariView.isAvailable();
-        if (available) {
-          SafariView.addEventListener("onShow", handleShow);
-          SafariView.addEventListener("onDismiss", handleClose);
-        }
-      } catch (err) {
-        available = false;
-      }
-
       _setSvEnable(enable === "true");
-      setSvAvailable(available);
     })();
-
-    return () => {
-      if (svAvailable) {
-        SafariView.removeEventListener("onShow", handleShow);
-        SafariView.removeEventListener("onDismiss", handleClose);
-      }
-    };
-  }, [svAvailable]);
+  }, []);
 
   const setSvEnable = React.useCallback((value) => {
     // Set state immediately to avoid switch UI delay
@@ -55,28 +25,26 @@ export const SettingsProvider = ({ children }) => {
 
   const openLink = React.useCallback(
     async (url, colors) => {
-      if (svAvailable && svEnable) {
-        // This is to avoid press multi times
-        if (svStarted) {
-          return;
-        }
-        setSvStarted(true);
-        SafariView.show({
-          url,
-          tintColor: colors.safari.text,
-          barTintColor: colors.safari.background,
+      if (Platform.OS === "ios" && svEnable) {
+        // TODO: This is to avoid press multi times
+        // if (svStarted) {
+        //   return;
+        // }
+
+        await WebBrowser.openBrowserAsync(url, {
+          controlsColor: colors.safari.text,
+          toolbarColor: colors.safari.background,
         });
       } else {
         Linking.openURL(url);
       }
     },
-    [svAvailable, svEnable, svStarted]
+    [svEnable]
   );
 
   return (
     <SettingsContext.Provider
       value={{
-        inSv,
         svAvailable,
         svEnable,
         setSvEnable,
